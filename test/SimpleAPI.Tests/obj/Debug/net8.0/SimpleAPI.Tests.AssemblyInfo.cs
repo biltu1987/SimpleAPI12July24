06f0d301b2f3b9f5a0e74377dd6477b4835f7340
biltu1987 <biltu.khawas@gmail.com>
2024-07-22 22:27:34 +0530

    updated

--- test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
+++ test/SimpleAPI.Tests/obj/Debug/net8.0/SimpleAPI.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a63900375314d345f5da6b49dac66b450d4e65a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleAPI.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
